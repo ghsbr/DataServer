e5d353123103824294ba4c06ef3f1cc29bdcb24d
Systool <systemtool.exe@gmail.com>
2020-01-15 20:16:34 +0100

    Aggiunte flag

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"flag"
 	"net"
 	"os"
 )
 
+var printDebug bool
+
 func main() {
+	var address string
+	flag.StringVar(
+		&address, "addr", "127.0.0.1:8080",
+		"Address at which the requests will be served",
+	)
+
+	flag.BoolVar(&printDebug, "debug", false, "Print Debug Messages")
+	help := flag.Bool("help", false, "Show help message and exit")
+	flag.Parse()
+
+	if *help {
+		print("DataServer v0.0.1\n\n")
+		flag.CommandLine.SetOutput(os.Stdout)
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if printDebug {
+		println("Debug Messages on")
+	}
+
 	server, err := net.Listen(
 		"tcp",
 		"127.0.0.1:8080",
