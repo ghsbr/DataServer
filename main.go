442f7162b1fb5f5e1aab4c2e3a154d64ca1e5db5
Systool <systemtool.exe@gmail.com>
2020-01-27 20:25:22 +0100

    [NOT WORKING] Finished performOneTimeSetup

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	//"net"
 	"os"
+	"time"
 
 	"github.com/bvinc/go-sqlite-lite/sqlite3"
 )
@@ -19,8 +20,8 @@ func main() {
 		"Address at which the requests will be served",
 	)
 
-	flag.BoolVar(&printDebug, "debug", false, "Print Debug Messages")
-	help := flag.Bool("help", true, "Show help message and exit")
+	flag.BoolVar(&printDebug, "debug", true, "Print Debug Messages")
+	help := flag.Bool("help", false, "Show help message and exit")
 	flag.Parse()
 
 	if *help {
@@ -34,7 +35,21 @@ func main() {
 		println("Debug Messages on")
 	}
 
-	//performOneTimeSetup()
+	conn, err := sqlite3.Open(":memory:")
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	mod, err := performOneTimeSetup(conn)
+	if err != nil {
+		println(err.Error())
+		os.Exit(2)
+	}
+	if printDebug {
+		println(mod)
+	}
 
 	/*server, err := net.Listen(
 		"tcp",
@@ -65,6 +80,59 @@ func main() {
 	}
 }*/
 
-func performOneTimeSetup(db sqlite3.Conn) {
+//TODO: usare una Session
+func performOneTimeSetup(db *sqlite3.Conn) (bool, error) {
+	//Prepariamo la query SQL
+	stmt, err := db.Prepare("SELECT name FROM sqlite_master WHERE type='table' AND name='long0'")
+	if err != nil {
+		return false, err
+	}
 
+	/*Eseguiamo la query e controlliamo exists per controllare se una riga
+	 *effettivamente esiste. In tal caso non agiremo e ritorneremo false
+	 *in caso contrario procederemo a creare le tabelle e a ritornare true*/
+	exists, err := stmt.Step()
+	stmt.Close()
+	if err != nil {
+		return false, err
+	} else if exists {
+		return false, nil
+	}
+
+	//Creo una tabella ogni 5 "gradi"
+	for i := -180; i <= 180; i += 5 {
+		err = db.Exec("CREATE TABLE long" + string(i) + ` (
+	long REAL NOT NULL
+	lat REAL NOT NULL
+	idx INTEGER NOT NULL
+	PRIMARY KEY (long lat)
+)`)
+		if err != nil {
+			return true, err
+		}
+	}
+
+	//Ottengo l'unix timestamp per la giornata di oggi
+	today := time.Now().UTC().Truncate(time.Duration(time.Hour * 24)).Unix()
+	if printDebug {
+		println(time.Unix(today, 0).Format(time.UnixDate))
+	}
+
+	//Creo una tabella per la giornata in corso
+	err = db.Exec("CREATE TABLE d" + string(today) + ` (
+	idx INTEGER NOT NULL
+	time INTEGER NOT NULL
+	pm25_concentration REAL
+	condition TEXT
+	humidity INTEGER
+	pressure INTEGER
+	wind_speed REAL
+	wind_direction INTEGER
+	temperature INTEGER
+	PRIMARY KEY(idx time)
+)`)
+	if err != nil {
+		return true, nil
+	}
+	return true, nil
 }
