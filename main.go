da3b48ea1aede0d9d21086953ec31fc605d7a1cf
Cleri Alberto <alberto.cleri4bii@gmail.com>
2020-02-12 17:16:00 +0100

    Provando INSERT INTO

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/bvinc/go-sqlite-lite/sqlite3"
 )
@@ -83,7 +82,50 @@ func main() {
 		fmt.Printf("%+v\n", out)
 	}
 
-	conn.Exec("INSERT OR REPLACE INTO long" + strconv.FormatInt(int64((out.Longitude+180)/5), 10) + " VALUES()")
+	{
+		stmt, err := conn.Prepare(
+			"SELECT id FROM long? WHERE long='?' AND lat='?'",
+			int64((out.Longitude+180)/5), out.Longitude, out.Latitude,
+		)
+		if err != nil {
+			println(err.Error())
+			os.Exit(6)
+		}
+		defer stmt.Close()
+
+		next, err := stmt.Step()
+		if err != nil {
+			println(err.Error())
+			os.Exit(7)
+		}
+
+		var id int64
+		if next {
+			id, ok, err := stmt.ColumnInt64(0)
+			if err != nil {
+				println(err.Error())
+				os.Exit(7)
+			}
+			if printDebug {
+				println(strconv.FormatBool(ok))
+			}
+		} else {
+
+		}
+	}
+	/*"CREATE TABLE long" + strconv.FormatInt(i, 10) + ` (
+		long REAL,
+		lat REAL,
+		idx INTEGER,
+		PRIMARY KEY (long, lat)
+	)`*/
+	/*"CREATE TABLE d" + strconv.FormatInt(today, 10) + ` (
+		idx INTEGER,
+		time INTEGER,
+		pm25_concentration REAL,
+		temperature INTEGER,
+		PRIMARY KEY(idx, time)
+	)`*/
 
 	/*server, err := net.Listen(
 		"tcp",
@@ -113,62 +155,3 @@ func main() {
 		println("Serving request sent by", conn.RemoteAddr().String())
 	}
 }*/
-
-//TODO: usare una Session
-func performOneTimeSetup(db *sqlite3.Conn) (bool, error) {
-	//Prepariamo la query SQL
-	stmt, err := db.Prepare("SELECT name FROM sqlite_master WHERE type='table' AND name='long0'")
-	if err != nil {
-		return false, err
-	}
-
-	/*Eseguiamo la query e controlliamo exists per controllare se una riga
-	 *effettivamente esiste. In tal caso non agiremo e ritorneremo false
-	 *in caso contrario procederemo a creare le tabelle e a ritornare true*/
-	exists, err := stmt.Step()
-	stmt.Close()
-	if err != nil {
-		return false, err
-	} else if exists {
-		return false, nil
-	}
-
-	//Creo una tabella ogni 5 "gradi"
-	{
-		var i int64
-		for i = 0; i < 360; i += 5 {
-			if printDebug {
-				println("Creating: long" + strconv.FormatInt(i, 10) + " for " + strconv.FormatInt(i-180, 10))
-			}
-			err = db.Exec("CREATE TABLE long" + strconv.FormatInt(i, 10) + ` (
-	long REAL,
-	lat REAL,
-	idx INTEGER,
-	PRIMARY KEY (long, lat)
-)`)
-			if err != nil {
-				return true, err
-			}
-		}
-	}
-
-	//Ottengo l'unix timestamp per la giornata di oggi
-	today := time.Now().UTC().Truncate(time.Duration(time.Hour * 24)).Unix()
-	if printDebug {
-		println(time.Unix(today, 0).Format(time.UnixDate))
-		println("Creating: d" + strconv.FormatInt(today, 10))
-	}
-
-	//Creo una tabella per la giornata in corso
-	err = db.Exec("CREATE TABLE d" + strconv.FormatInt(today, 10) + ` (
-	idx INTEGER,
-	time INTEGER,
-	pm25_concentration REAL,
-	temperature INTEGER,
-	PRIMARY KEY(idx, time)
-)`)
-	if err != nil {
-		return true, err
-	}
-	return true, nil
-}
