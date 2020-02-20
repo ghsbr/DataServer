cbd20b9cbf0da6a0c617343f4b8e2fb73b7f09e9
Systool <systemtool.exe@gmail.com>
2020-02-20 21:48:20 +0100

    Test praticamente finiti

--- database_test.go
+++ database_test.go
@@ -10,7 +10,8 @@ import (
 var db Database
 
 func TestNewDatabase(dc *testing.T) {
-	db, _, err := NewDatabase(":memory:")
+	var err error
+	db, _, err = NewDatabase(":memory:")
 	if err != nil {
 		dc.Errorf("Error while creating Database: %v", err)
 	}
@@ -48,7 +49,29 @@ func TestPreciseQuery(dc *testing.T) {
 	if err != nil {
 		dc.Errorf("Error found %v", err)
 	} else {
-		dc.Logf("PreciseQuery returned: %+v", el)
+		dc.Logf("ApproximateQuery returned: %+v", el)
+	}
+}
+
+func TestApproximateQueryInRange(dc *testing.T) {
+	els, err := db.ApproximateQuery(out.Longitude, out.Latitude, time.Now().UTC().Truncate(time.Hour*24).Unix(), 100)
+	if err != nil {
+		dc.Errorf("Error found %v", err)
+	} else if len(els) > 0 {
+		dc.Logf("ApproximateQuery returned: %+v", els)
+	} else {
+		dc.Error("ApproximateQuery didn't return a Data")
+	}
+}
+
+func TestApproximateQueryOutOfRange(dc *testing.T) {
+	els, err := db.ApproximateQuery(out.Longitude-50, out.Latitude+50, time.Now().UTC().Truncate(time.Hour*24).Unix(), 100)
+	if err != nil {
+		dc.Errorf("Error found %v", err)
+	} else if len(els) > 0 {
+		dc.Logf("ApproximateQuery returned: %+v", els)
+	} else {
+		dc.Error("ApproximateQuery didn't return a Data")
 	}
 }
 
